18df055b9d3e9c9ab83384626cb80cca5a1807f9
Jennifer Russell <jc.russell@yahoo.com>
2020-07-22 17:11:19 -0400

    Fixed output of calcuator to limit to two decimals

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-
 function tipTotal() {
 let billAmount = document.getElementById("amount").value;
 let numberOfPeople = document.getElementById("numOfPeople").value;
 let tipAmount = document.getElementById("service").value;
+    if (billAmount === 0 || billAmount === "") {
+        alert("Enter Bill Amount")
+    } else if (numberOfPeople === 0 || numberOfPeople === "") {
+        alert("Enter number of people splitting the tip")
+    } else if (tipAmount  {
+        alert("Enter service level")
+    }
 let tipMath = billAmount * tipAmount / numberOfPeople ;
-document.getElementById("placehold").innerHTML = "TIP AMOUNT: <br>" + "$ " + tipMath + "<br>Per person";
+document.getElementById("placehold").innerHTML = "TIP AMOUNT: <br>" + "$ " + tipMath.toFixed(2) + "<br>Per person";
 }
